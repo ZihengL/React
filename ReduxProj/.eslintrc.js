e5bed22b0efc3d48560d6f450f482def27064e62
Zi heng Liu <liu.ziheng@yahoo.ca>
2024-03-17 18:13:14 -0400

    a

--- ReduxProj/.eslintrc.js
+++ ReduxProj/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  quotes: [2, 'single'],
+  globals: {
+    SwaggerEditor: false,
+  },
+  env: {
+    browser: true,
+  },
+  rules: {
+    'linebreak-style': 0, // <----------
+  },
 };
